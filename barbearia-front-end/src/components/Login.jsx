88415cee6e629e4e9a42eff3c7b7ada46ed086c2
Luis Miguel <luiskasami@gmail.com>
2026-03-05 16:52:16 -0300

    login ok

--- barbearia-front-end/src/components/Login.jsx
+++ barbearia-front-end/src/components/Login.jsx
@@ -2,41 +2,53 @@ import { useState } from "react";
 import BgImgBlur from "../assets/BgImgBlur.png";
 import "./Login.css";
 import logo from "../assets/logo.png";
+import olhoAberto from "../assets/olhoAberto.png";
+import olhoFechado from "../assets/olhoFechado.png";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [mostrarSenha, setMostrarSenha] = useState(false);
   return (
     <div className="login-container">
       <img src={BgImgBlur} alt="fundo" className="bg-image" />
-        <div className="login-box">
-          
-          <form className="login-form">
-            <h2>Faça Login</h2>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              placeholder="Digite seu e-mail"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="login-box"
+      <form className="login-form">
+        <h2>Faça Login</h2>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Digite seu e-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="login-box"
+        />
+        <div className="input-senhaContainer">
+          <input
+            type={mostrarSenha ? "text" : "password"}
+            name="password"
+            id="password"
+            placeholder="Digite sua senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="password-box"
+          />
+          <button
+            type="button"
+            className="btn-eye"
+            onClick={() => setMostrarSenha(!mostrarSenha)}
+          >
+            <img
+              src={mostrarSenha ? olhoAberto : olhoFechado}
+              alt={mostrarSenha ? "Ocultar senha" : "Mostrar senha"}
             />
-            <input
-              type="password"
-              name="password"
-              id="password"
-              placeholder="Digite sua senha"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="password-box"
-            />
-            <button type="submit" className="button-entrar">
-              Entrar
-            </button>
-            <button className="button-cadastro">Cadastrar-se</button>
-          </form>
-      </div>
+          </button>
+        </div>
+        <button type="submit" className="button-entrar">
+          Entrar
+        </button>
+        <button className="button-cadastro">Cadastrar-se</button>
+      </form>
       <img src={logo} alt="logo" className="img-logo" />
     </div>
   );
